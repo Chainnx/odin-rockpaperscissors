9a00117769940d9713ddc50ec4baaef18307e71b
Anthony Tiu <helloworldpns@gmail.com>
2023-04-25 12:30:25 +0800

    added if else rps

--- code.js
+++ code.js
@@ -3,3 +3,26 @@ function getComputerChoice() {
   let choice = ["rock", "paper", "scissors"];
   return choice[Math.floor(Math.random() * choice.length)];
 }
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return "Equal";
+  }
+}
+
+let playerSelection = "Rock";
+playerSelection = playerSelection.toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
